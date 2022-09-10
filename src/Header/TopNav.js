5cb3f56d56f3e8de345ba92f114d0a5614fa79fd
Oghenekparobo <classicsteph11@gmail.com>
2022-09-10 02:39:51 +0100

    refactored tailwindcss codes on TopNav component

--- src/Header/TopNav.js
+++ src/Header/TopNav.js
@@ -1,8 +1,8 @@
 const TopNav = () => {
   return (
     // content on top of the page
-    <div className="top-nav  bg-slate-100 text-center  text-gray-500 text-sm  flex justify-center items-center flex-col space-y-2 py-4 md:space-y-0 md:flex-row md:justify-between  md:px-12 md:py-2">
-      <div className="contact space-y-2 md:space-y-0 md:flex md:items-center">
+    <div className="top-nav  bg-slate-100 text-center  text-gray-500 text-sm  flex-items__center justify-center  flex-col space-y-2 py-4 md:space-y-0 md:flex-row md:justify-between  md:px-12 md:py-2">
+      <div className="contact space-y-2 md:space-y-0 md-flex-items__center">
         <p>
           Order On Phone:
           <span className="text-lime-500 font-bold px-2">
@@ -11,7 +11,7 @@ const TopNav = () => {
         </p>
         {/* dotted line partiton on top nav */}
         <div className="dot-line hidden md:block"></div>
-        <p className="flex items-center">
+        <p className="flex-items__center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -31,68 +31,67 @@ const TopNav = () => {
           </span>
         </p>
       </div>
-      <div className="reach md:flex md:items-center">
-        <div className="flex items-center space-x-6 md:space-x-0 md:pr-0">
-        <p  className="flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-            />
-          </svg>
-          <span className="px-2">Account</span>
-        </p>
-        <div className="dot-line hidden md:block"></div>
-        <p className="flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-            />
-          </svg>
+      <div className="reach md-flex-items__center">
+        <div className="flex-items__center space-x-6 md:space-x-0 md:pr-0">
+          <p className="flex-items__center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+              />
+            </svg>
+            <span className="px-2">Account</span>
+          </p>
+          <div className="dot-line hidden md:block"></div>
+          <p className="flex-items__center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+              />
+            </svg>
 
-          <span class="px-2">Wishlist</span>
-        </p>
+            <span class="px-2">Wishlist</span>
+          </p>
         </div>
-        
-        <div className="dot-line hidden md:block"></div>
-        <div className="mx-8 md:mx-0">
-        <p className="flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
-            />
-          </svg>
 
-          <span className="px-2">Track Order</span>
-        </p>
+        <div className="dot-line hidden md:block"></div>
+        <div className="mx-8 pt-3 md:pt-0 md:mx-0">
+          <p className="flex-items__center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
+              />
+            </svg>
+
+            <span className="px-2">Track Order</span>
+          </p>
         </div>
-        
       </div>
     </div>
   );
