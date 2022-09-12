9ecb7a0cd7323e180d8bf7d5b3da1d983a46dc90
STEPHEN ONOKWAKPOR <classicsteph11@gmail.com>
2022-09-12 14:00:25 +0100

    production section for dsktop view

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -69,10 +69,10 @@ const Products = () => {
           </button>
         </div>
       </div>
-      <div className=" relative grid grid-cols-1 ml-6 space-y-4 pt-4 md:space-y-0 text-center md:grid-cols-4">
+      <div className=" relative grid grid-cols-1 space-y-4 pt-4 md:space-y-0 text-center md:grid-cols-4">
         {products.map((product) => (
           <div
-            className="prod-card border-2 border-opacity-25 p-4 text-center w-5/6 cursor-pointer relative"
+            className="prod-card border-2 border-opacity-25 p-4 text-center w-11/12 md:w-5/6 cursor-pointer relative"
             onMouseOver={() => setBgOverlay(true)}
             onMouseOut={() => {
               setBgOverlay(false);
