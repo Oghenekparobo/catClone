98719c59f543473dc900a6513cee5568c05b7a0c
STEPHEN ONOKWAKPOR <classicsteph11@gmail.com>
2022-09-15 13:23:27 +0100

    working on feature section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Cart from "./components/Cart/Cart";
+import Features from "./components/Features/Features";
 import Top from "./components/Header/Top";
 import Products from "./components/Products/Products";
 import Provider from "./store/Provider";
@@ -12,10 +13,15 @@ function App() {
   return (
     <Provider>
       {viewCart && <Cart removeCart={removeCartHandler} />}
+      {/* menu and sliders */}
       <Top onCart={showCartHandler} />
       <div className="product-section">
         <Products />
       </div>
+      <div className="features-sections">
+      <Features/>
+      </div>
+   
     </Provider>
   );
 }
---
+++ src/components/Features/Features.js
@@ -0,0 +1,21 @@
+import grass from "../../assets/grass.png";
+const Features = () => {
+  return (
+    <section>
+      <div className=" py-8 text-center bg-fuchsia-100">
+        <h1 className="font-bold text-2xl pt-10">WHY  CHOOSE US</h1>
+        <div className="flex flex-col justify-center items-center space-y-4 py-4">
+          <img src={grass} alt="grass" width="35" />
+          <p className="opacity-50 font-bold max-w-5xl">
+          THERE ARE MANY VARIATIONS OF PASSAGES OF LOREM IPSUM AVAILABLE, BUT
+          THE MAJORITY HAVE SUFFERED ALTERATION IN SOME FORM, BY INJECTED
+          HUMOUR.
+        </p>
+        </div>
+      
+      </div>
+    </section>
+  );
+};
+
+export default Features;
